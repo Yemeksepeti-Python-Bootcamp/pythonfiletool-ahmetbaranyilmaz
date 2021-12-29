cc7670a48cfc15d48a80d753b98c01754cd1c863
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-12-29 16:48:57 +0300

    ADD search function

--- FileTool.py
+++ FileTool.py
@@ -1,1 +1,10 @@
+class FileOperations:
+    def __init__(self, path, fields, *args, **kwargs):
+        self.path = path
+        self.fields = fields
+
+    def search(self, keyword):
+        with open(self.path, 'r') as file:
+            return [(i+1, r.replace('\n', '')) for i,r in enumerate(file.readlines()[1:]) if keyword.lower() in r.lower()]
+
 
