885c3006c99e4a40047c8e780257f7b1a307f112
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-12-29 16:50:52 +0300

    ADD deleteByIndex function

--- FileTool.py
+++ FileTool.py
@@ -15,6 +15,16 @@ class FileOperations:
             file.write(header)
             file.writelines(data)
 
+    def __deleteByIndex(self, deletePointer):
+        with open(self.path, 'r') as file:
+            header = file.readline()
+            data = [row for i, row in enumerate(file.readlines()) if i not in deletePointer]
+        with open('test.csv', 'w') as file:
+            file.write(header)
+            file.writelines(data)
+
     def delete(self, deletePointer, by='keyword'):
         if by == 'keyword':
             self.__deleteByKeyword(deletePointer)
+        if by == 'index':
+            self.__deleteByIndex(deletePointer)
