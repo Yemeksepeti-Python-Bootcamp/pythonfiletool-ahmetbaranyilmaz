129c24ad1beaf11fc76f5bc3ddb8d2059532bd31
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-12-29 16:56:23 +0300

    CHANGE deletePointer to deleteKey

--- FileTool.py
+++ FileTool.py
@@ -7,24 +7,24 @@ class FileOperations:
         with open(self.path, 'r') as file:
             return [(i+1, r.replace('\n', '')) for i,r in enumerate(file.readlines()[1:]) if keyword.lower() in r.lower()]
 
-    def __deleteByKeyword(self, deletePointer): # fileOps.delete('sport')
+    def __deleteByKeyword(self, deleteKey): # fileOps.delete('sport')
         with open(self.path, 'r') as file:
             header = file.readline()
-            data = [row for row in file.readlines() if deletePointer.lower() not in row.lower()]
+            data = [row for row in file.readlines() if deleteKey.lower() not in row.lower()]
         with open(self.path, 'w') as file:
             file.write(header)
             file.writelines(data)
 
-    def __deleteByIndex(self, deletePointer): # fileOps.delete([5,7,9,10,11,22,2,3,4], by='index')
+    def __deleteByIndex(self, deleteKey): # fileOps.delete([5,7,9,10,11,22,2,3,4], by='index')
         with open(self.path, 'r') as file:
             header = file.readline()
-            data = [row for i, row in enumerate(file.readlines()) if i not in deletePointer]
-        with open('test.csv', 'w') as file:
+            data = [row for i, row in enumerate(file.readlines()) if i not in deleteKey]
+        with open(self.path, 'w') as file:
             file.write(header)
             file.writelines(data)
 
-    def delete(self, deletePointer, by='keyword'):
+    def delete(self, deleteKey, by='keyword'):
         if by == 'keyword':
-            self.__deleteByKeyword(deletePointer)
+            self.__deleteByKeyword(deleteKey)
         if by == 'index':
-            self.__deleteByIndex(deletePointer)
+            self.__deleteByIndex(deleteKey)
