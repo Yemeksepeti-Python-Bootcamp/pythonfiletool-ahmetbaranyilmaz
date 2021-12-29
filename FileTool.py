6fcd02d334dafad87c24658ccd7cd05645386431
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-12-29 16:50:06 +0300

    ADD private deleteByKeyword function

--- FileTool.py
+++ FileTool.py
@@ -7,4 +7,14 @@ class FileOperations:
         with open(self.path, 'r') as file:
             return [(i+1, r.replace('\n', '')) for i,r in enumerate(file.readlines()[1:]) if keyword.lower() in r.lower()]
 
+    def __deleteByKeyword(self, deletePointer):
+        with open(self.path, 'r') as file:
+            header = file.readline()
+            data = [row for row in file.readlines() if deletePointer.lower() not in row.lower()]
+        with open(self.path, 'w') as file:
+            file.write(header)
+            file.writelines(data)
 
+    def delete(self, deletePointer, by='keyword'):
+        if by == 'keyword':
+            self.__deleteByKeyword(deletePointer)
